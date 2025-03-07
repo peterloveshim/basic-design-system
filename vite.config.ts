745b06dbdbff8cb512bc6efc15e0384d8fd08c31
peterloveshim <peterloveshim@gmail.com>
2025-03-07 10:13:06 +0900

    fix: changed vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      name: "react-beautiful-timeline",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  /*
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  */
+});
